42592e0ffad714614e077536f7055ac1fda36c32
truddy <truddy@umich.edu>
2020-05-21 04:12:38 -0400

    Got Solver and Promising function working. Gives correct output just need to clean up some stuff

--- sudoku_solver/solver.cpp
+++ sudoku_solver/solver.cpp
@@ -12,13 +12,17 @@
 
 using namespace std;
 
-bool is_promising(vector<vector<int>>& board, int row, int col);
+bool violation(vector<vector<int>>& board, int row, int col);
 
-void solver(vector<vector<int>>& board, int row, int col, bool solution);
+void solver(vector<vector<int>>& board, int row, int col, bool& solution);
 
 void print_grid(vector<vector<int>>& board);
 
-int main(/*int argc, const char * argv[]*/) {
+void update_square(int& row, int& col);
+
+void revert_square(int& row, int& col);
+
+int main() {
     vector<vector<int>> example = { { 0, 3, 0, 0, 1, 0, 0, 6, 0 },
                                     { 7, 5, 0, 0, 3, 0, 0, 4, 8 },
                                     { 0, 0, 6, 9, 8, 4, 3, 0, 0 },
@@ -28,42 +32,80 @@ int main(/*int argc, const char * argv[]*/) {
                                     { 0, 0, 1, 6, 7, 5, 2, 0, 0 },
                                     { 6, 8, 0, 0, 9, 0, 0, 1, 5 },
                                     { 0, 9, 0, 0, 4, 0, 0, 3, 0 }};
-    
-    print_grid(example);
+    bool solution = false;
+    solver(example, 0, 0, solution);
+    if (solution)
+        print_grid(example);
+    else
+        cout << "No Solution!\n";
     return 0;
 }
 
-bool is_promising(vector<vector<int>>& board, int row, int col) {
+bool violation(vector<vector<int>>& board, int row, int col) {
+    revert_square(row, col);
+    int curr_val = board[row][col];
+    if (curr_val == 0)
+        return false;
     
+    // Check other rows to see if violation
+    for (int r = 0; r < 8; ++r) {
+        if (r != row) {
+            if (board[r][col] == curr_val)
+                return true;
+        }
+    } // for r
+    
+    // Check other columns to see if violation
+    for (int c = 0; c < 8; ++c) {
+        if (c != col) {
+            if (board[row][c] == curr_val)
+                return true;
+        }
+    } // for r
+    
+    // Check 3x3 box for violation
+    int r_pos = row / 3;
+    int c_pos = col / 3;
+    for (int r = r_pos * 3; r < (r_pos + 1) * 3; ++r) {
+        for (int c = c_pos * 3; c < (c_pos + 1) * 3; ++c) {
+            if (row != r && col != c) {
+                if (curr_val == board[r][c])
+                    return true;
+            }
+            
+        } // for f
+    } // for r
+    
+    // If no violations, return false
     return false;
-} // is_promising();
+} // violation();
 
-void solver(vector<vector<int>>& board, int row, int col, bool solution) {
-    // if promising
-    if (row == 8 && col == 8) {
+void solver(vector<vector<int>>& board, int row, int col, bool& solution) {
+    // If no violations
+    if (violation(board, row, col))
+        return;
+    
+    
+    // If end of puzzle
+    if (row == 9 && col == 0) {
         solution = true;
         return;
     } // if end of puzzle
     
-    
-    // Update row and column to next position
-    if (col == 8) {
-        row++;
-        col = 0;
-    } // if
-    else
-        col++;
-    
-    if (board[row][col] != 0)
+    if (board[row][col] != 0) {
+        update_square(row, col);
         solver(board, row, col, solution);
+    }
     else {
         for (int val = 1; val < 10; ++val) {
             board[row][col] = val;
+            update_square(row, col);
             solver(board, row, col, solution);
-            
+            revert_square(row, col);
             if (solution)
                 return;
         } // for i
+        board[row][col] = 0;
     } // else
 } // solver()
 
@@ -80,3 +122,21 @@ void print_grid(vector<vector<int>>& board) {
         cout << endl;
     } // for row
 } // print_grid()
+
+void update_square(int& row, int& col) {
+    if (col == 8) {
+        col = 0;
+        row++;
+    }
+    else
+        col++;
+} // update_square()
+
+void revert_square(int& row, int& col) {
+    if (col == 0 && row != 0) {
+        col = 8;
+        row--;
+    }
+    else if (col != 0)
+        col--;
+}
