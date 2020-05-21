befba9b63bfbd6721dcb484127405fe17f34e744
truddy <truddy@umich.edu>
2020-05-21 05:38:45 -0400

    Added diagonal option

--- sudoku_solver/solver.cpp
+++ sudoku_solver/solver.cpp
@@ -18,9 +18,13 @@ void solver(int (&board)[9][9], int row, int col, bool& solution);
 
 void print_board(int (&board)[9][9]);
 
+bool diagonal = false;
+
+bool knight = false;
+
 int main() {
     cout << setprecision(5) << fixed;
-    int easy[9][9] = { { 0, 3, 0, 0, 1, 0, 0, 6, 0 },
+    /*int easy[9][9] = { { 0, 3, 0, 0, 1, 0, 0, 6, 0 },
                          { 7, 5, 0, 0, 3, 0, 0, 4, 8 },
                          { 0, 0, 6, 9, 8, 4, 3, 0, 0 },
                          { 0, 0, 3, 0, 0, 0, 8, 0, 0 },
@@ -28,7 +32,7 @@ int main() {
                          { 0, 0, 4, 0, 0, 0, 5, 0, 0 },
                          { 0, 0, 1, 6, 7, 5, 2, 0, 0 },
                          { 6, 8, 0, 0, 9, 0, 0, 1, 5 },
-                         { 0, 9, 0, 0, 4, 0, 0, 3, 0 }};
+                         { 0, 9, 0, 0, 4, 0, 0, 3, 0 }};*/
 
     // MAYBE TRY TO CODE DIAGONAL AND KNIGHT CHESS
     /*int hard[9][9] = {{ 8, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -41,11 +45,23 @@ int main() {
                       { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
                       { 0, 9, 0, 0, 0, 0, 4, 0, 0 }};*/
     
+    int diagonal[9][9] = {{ 0, 4, 0, 3, 0, 0, 0, 8, 0 },
+                            { 0, 0, 3, 5, 0, 2, 0, 4, 0 },
+                            { 0, 0, 0, 0, 0, 1, 5, 0, 3 },
+                            { 0, 0, 2, 4, 0, 0, 1, 0, 0 },
+                            { 4, 0, 0, 0, 2, 0, 0, 0, 7 },
+                            { 0, 0, 7, 0, 0, 3, 8, 0, 0 },
+                            { 1, 0, 8, 2, 0, 0, 0, 0, 0 },
+                            { 0, 7, 0, 1, 0, 8, 4, 0, 0 },
+                            { 0, 2, 0, 0, 0, 9, 0, 1, 0 }};
+    
     bool solution = false;
     auto start = chrono::high_resolution_clock::now();
-    solver(easy, 0, 0, solution);
+    solver(diagonal, 0, 0, solution);
+    
+    
     if (solution)
-        print_board(easy);
+        print_board(diagonal);
     else
         cout << "No Solution!\n";
     auto end = chrono::high_resolution_clock::now();
@@ -84,6 +100,27 @@ bool violation(int (&board)[9][9], int row, int col) {
             
         } // for f
     } // for r
+    
+    // If diagonal puzzle
+    if (diagonal) {
+        if (row == col) {
+            for (int i = 0; i < 9; ++i) {
+                if (i != row) {
+                    if (board[i][i] == curr_val)
+                        return true;
+                }
+            } // for i
+        } /* if \ */
+        
+        if (row + col == 8) {
+            for (int i = 0; i < 9; ++i) {
+                if (i != row) {
+                    if (board[i][8 - i] == curr_val)
+                        return true;
+                }
+            } // for i
+        } // if /
+    } // if diagonal puzzle
     
     // If no violations, return false
     return false;
