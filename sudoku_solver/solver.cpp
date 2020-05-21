1a22956c6dd40ad6138f647204b5f73db1f33fd6
truddy <truddy@umich.edu>
2020-05-21 02:19:06 -0400

    Initialize

---
+++ sudoku_solver/solver.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sudoku_solver
+//
+//  Created by Tyler Ruddy on 5/21/20.
+//  Copyright © 2020 Tyler Ruddy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
